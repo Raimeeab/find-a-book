dba022a7854e7b05be7cb2b3e305b80784dadc57
Raimeeab <raimee.abbassi@gmail.com>
2022-02-12 12:25:05 +1100

    Create add user/ login mutations & findUser

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,55 @@
+const {
+  AuthenticationError,
+  UserInputError,
+} = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    getUser: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
+      const correctPw = await user.isCorrectPassword(password);
+
+      // If email or password is incorrect, throw the same error
+      if (!user || !correctPw) {
+        throw new AuthenticationError("Incorrect login credentials");
+      }
+
+      // When user is successfully logged in:
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+  Mutation: {
+    createUser: async (parent, { userInput }) => {
+      let { username, email, password, confirmPassword } = userInput;
+
+      // Use validators to ensure user input meets criteria
+      const { valid, errors } = validateUserInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+
+      if (!valid) {
+        throw new UserInputError("Validation errors", { errors });
+      };
+
+      // Allows for app to be used in social network context in the future
+      let user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username already exists", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      };
+
+      user = await User.create(userInput);
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,34 @@
 const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+
+type Book {
+    authors: String
+    description: String!
+    bookId: String! 
+    image: String
+    link: String 
+    title: String!
+}
+
+type User { 
+    username: String!
+    email: String! 
+    password: String! 
+    savedBooks: [Book]
+} 
+
+type Auth {
+    token: ID!
+    user: User
+} 
+
+input UserInput {
+    username: String! 
+    email: String! 
+    password: String! 
+    confirmPassword: String! 
+}
+
+
+`;
